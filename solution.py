6fa1be96d0da388a229653a251c8e05b1debc374
Victoria <vmoix001@mail.ru>
2020-03-16 21:17:21 +0700

    first

---
+++ solution.py
@@ -0,0 +1,7 @@
+# Case-study 4
+# Developers: Vlasov V., Moiseenko V.
+# The aim: to develop a program for generating the number of offers from a file using Markov algorithms
+
+import random
+
+# For Victoria:
