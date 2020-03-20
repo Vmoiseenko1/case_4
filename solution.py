1d45c43245faa2b91d549125e23562489239f17a
Victoria <vmoix001@mail.ru>
2020-03-21 00:59:01 +0700

    start_words

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-# Case-study 4
-# Developers: Vlasov V., Moiseenko V.
+# Case-study 6 "Generating of sentences"
+# Developers: Vlasov V. (%)
+#             Moiseenko V. (%)
 # The aim: to develop a program for generating the number of offers from a file using Markov algorithms
 
 import random
@@ -20,13 +21,14 @@ def check_text(file): # to check the text
             lines[z] = lines[z].replace(' ,', ',')
         return lines
 
-file = input('Имя файла: ')
-print(check_text(file))
-
 # For Victoria:
-def start_words(): #получить список стартовых слов
-    pass
-
+def start_words(text): #получить список стартовых слов
+    start_words = []
+    for word in text:
+        lst = word.split()
+        for w in lst:
+            start_words.append(w)
+    return start_words
 
 # For Vlas:
 def get_parts(): #получить звенья
@@ -36,3 +38,9 @@ def get_parts(): #получить звенья
 # For Vlas:
 def text_generating(): #сгенерировать текст
     pass
+
+
+file = input('Имя файла: ')
+text = check_text(file)
+print(start_words(text))
+print(check_text(file))
